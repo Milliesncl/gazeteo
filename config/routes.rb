693d6de7c58458db5e7a8cf21d146eb4b39ea7cf
Millie Senecal <millie.senecal@gmail.com>
2020-10-13 10:39:42 -0400

    routes for posts

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get "welcome", to: 'pages#home'
   root to: 'pages#home'
 
-
+  resources :posts, except: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
