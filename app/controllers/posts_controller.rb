047e6045ff18baba3c66b73aa84aae0210d6521f
Millie Senecal <millie.senecal@gmail.com>
2020-10-15 10:04:06 -0400

    crud action finished for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,7 +23,7 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     if @post.update(post_params)
-      redirect_to post_path
+      redirect_to posts_path
     else
       render :edit
     end
