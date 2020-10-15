57b0efd7f45afea44fa6fdcb62d01feb71be6efa
Millie Senecal <millie.senecal@gmail.com>
2020-10-15 09:45:33 -0400

    destroy a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,12 +17,25 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      redirect_to posts_path
+    else
+      render :delete
+    end
   end
 
 private
