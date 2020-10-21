61df321f592ba177d3da2afb8cbd0fd2c3e3ddf6
Millie Senecal <millie.senecal@gmail.com>
2020-10-21 14:41:16 -0400

    add validations to members

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,5 +1,6 @@
 class Family < ApplicationRecord
   has_many :family_users
   has_many :users, through: :family_users
+  has_many :members
   has_one_attached :photo
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,6 @@
 class Member < ApplicationRecord
+    has_one_attached :photo
+    belongs_to :family
+
+    validates :first_name, :last_name, :address, :birthday, presence: true
 end
