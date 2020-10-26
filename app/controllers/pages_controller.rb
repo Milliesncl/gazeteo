8ace8c709597f4dc745752f12766dc0b78a1107e
Millie Senecal <millie.senecal@gmail.com>
2020-10-26 16:26:30 -0400

    home adding info

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,16 @@
 class PagesController < ApplicationController
   def home
     @posts = current_user.posts
+    @families = current_user.families
+    @members = current_user.families.first.members
+  end
+
+  def create_post
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 end
